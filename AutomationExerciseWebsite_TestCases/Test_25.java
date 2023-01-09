16b4c185ed6d51c356e1296f05518c6978526943
İsmail ASLAN <ismailaslan190525@gmail.com>
2023-01-09 21:47:43 +0300

    Verify Scroll Up using 'Arrow' button and Scroll Down functionality

---
+++ AutomationExerciseWebsite_TestCases/Test_25.java
@@ -0,0 +1,68 @@
+package AutomationExerciseWebpage_Exercises;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Test_25 {
+    WebDriver driver=new ChromeDriver();
+    @BeforeTest
+    public void launchBrowserAndNavigateWebsite(){
+        //        1. Launch browser
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //        2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+    }
+
+    @Test(priority = 1)
+    public void verifyHomepage(){
+        //        3. Verify that home page is visible successfully
+        WebElement logo=driver.findElement(By.xpath("//img[@alt='Website for automation practice']"));
+        Assert.assertTrue(logo.isDisplayed());
+    }
+    @Test(priority = 2)
+    public void scrollDownToBottom() throws InterruptedException {
+        //      4. Scroll down page to bottom
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
+    }
+    @Test(priority = 3)
+    public void verifyText(){
+        //    5. Verify text 'SUBSCRIPTION'
+        WebElement text= driver.findElement(By.xpath("//h2[normalize-space()='Subscription']"));
+        Assert.assertTrue(text.isDisplayed());
+    }
+    @Test(priority = 4)
+    public void clickOnArrow(){
+        //        6. Click on arrow at bottom right side to move upward
+
+        WebDriverWait w =new WebDriverWait(driver,Duration.ofSeconds(10));
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='scrollUp']")));
+        driver.findElement(By.xpath("//a[@id='scrollUp']")).click();
+    }
+    @Test(priority = 5)
+    public void verifyPageIsScrolledUpAndText(){
+        //        7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
+        WebElement upperText= driver.findElement(By.xpath("//div[@class='item active']//h2[contains(text(),'Full-Fledged practice website for Automation Engin')]"));
+        Assert.assertTrue(upperText.isDisplayed());
+    }
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+    }
+}
+
