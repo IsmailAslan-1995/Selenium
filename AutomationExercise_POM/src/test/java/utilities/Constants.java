1f2d0f417f7e10891f8c2ffccb157eea1ebce75f
İsmail ASLAN <ismailaslan190525@gmail.com>
2023-03-02 23:53:51 +0300

    .

---
+++ AutomationExercise_POM/src/test/java/utilities/Constants.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import java.util.List;
+
+public class Constants {
+    public  static   final String NAME="Ismail";
+    public  static   final String LASTNAME="ASLAN";
+    public  static   final String SIGN_UP_EMAIL ="QWERT1905@gmail.com";
+    public  static   final String SIGN_UP_PASSWORD ="12345iso";
+    public  static   final String LOGIN_EMAIL ="QWERT1905@gmail.com";
+    public  static   final String LOGIN_PASSWORD ="12345iso";
+
+    public  static   final String BIRTHDATE="12-April-1995";
+    public  static   final String COMPANY_NAME="Inar Academy";
+    public  static   final String ADDRESS="Pınar Street no:4 floor:2";
+    public  static   final String ADDRESS_SECOND="Ayvalı Street no:25 floor:5";
+    public  static   final String COUNTRY="United States";
+    public  static   final String STATE="Dogu Anadolu";
+    public  static   final String CITY="Erzurum";
+    public  static   final String ZIPCODE="25400";
+    public  static   final String MOBILE_NUMBER="123456789";
+    public  static   final String SUBJECT="Products";
+    public static final String MESSAGE="Products which purchase it is damaged.";
+    public static final String UPLOAD_FILEPATH="C:\\Users\\Sys\\Downloads\\code.txt";
+    public static final  String SEARCHED_PRODUCT_NAME="Fancy Green Top";
+    public static final int SEARCHED_PRODUCT_NUMBER=3;
+    public static final String [] SEARCHED_PRODUCT_NAME_ARRAY={"Soft Stretch Jeans","Regular Fit Straight Jeans","Grunt Blue Slim Fit Jeans"};
+    public static final  String ADDED_PRODUCT_NAME="Blue Top";
+}
